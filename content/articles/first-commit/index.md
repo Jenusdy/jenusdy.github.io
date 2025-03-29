ff747c36c3d6a4ae5e5f2704b2c97a872ad3815b
Fawcet Jenusdy Makay <jenusdy@gmail.com>
2025-03-29 18:22:10 +0700

    Push Local

---
+++ content/articles/first-commit/index.md
@@ -0,0 +1,26 @@
+---
+title: "First Commit."
+description: "This is first commit for this blog section on my wesbite."
+date: "2025-03-29"
+banner:
+  src: "../../images/articles/first-commit/thumbnail.webp"
+  alt: "First Commit"
+categories:
+  - "Blog"
+  - "Random"
+keywords:
+  - "First"
+  - "Commit"
+  - "Blog"
+---
+
+## Hello There
+
+```kotlin
+  fun main() {
+    println("Hello, World! This is the initial commit.")
+  }
+```
+
+This marks the **initial commit** of my personal portfolio built using **Gatsby**. This project serves as a space where I showcase my skills, projects, and contributions in **web development, data science, GIS, and software engineering**.
+
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,10 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://jenusdy.github.io/", // Used for sitemap generation
         manifestSettings: {
-          favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
+          favicon: "./content/images/jenusdy.jpg", // Path is relative to the root
+          siteName: "Jenusdy's Portfolio", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
@@ -23,7 +23,7 @@ module.exports = {
         //     anonymize: true, // Default true
         //     environments: ["production", "development"] // Default ["production"]
         // }
-      },
-    },
+      }
+    }
   ],
 };
